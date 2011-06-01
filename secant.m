7990371e4b45852a5ed38cea3c8ce6773ba11fea
mohamed.elsaka2007@gmail.com <mohamed.elsaka2007@gmail.com@79f63fb2-dd47-5393-0cc9-0ffa0bf15a7b>
2011-06-01 04:55:42 +0000

    Secant Method.

---
+++ secant.m
@@ -0,0 +1,45 @@
+function [root,yRoot,err,it,P] = secant(f,a,b,delta)
+% Inputs
+%   f       name of the function
+%   a       left endpoint
+%   b       right endpoint
+%   delta   convergence tolerance
+% Return
+%   c       solution: the root
+%   yc      function value at c
+%   err     error estimate in c
+%   it      number of iterations
+%   P       History vector of the iterations
+P = [a b];
+ya = feval(f,a);
+yb = feval(f,b);
+
+if ya*yb > 0,
+    return
+end
+
+while abs(b-a) > delta ,
+%     if a > b,
+%         temp = a;
+%         a = b;
+%         b = temp;
+%     end;
+    ya = feval(f,a);
+    yb = feval(f,b);
+    temp = b - ( ( yb / (yb - ya ) ) * (b - a) );
+    if temp > a
+        disp 1;
+        a = temp;
+    else
+       disp 2;
+       b = a;
+       a = temp;
+    end 
+    P = [P;a b];
+end
+it = length(P);
+root  = a
+yRoot = feval(f,root);
+err = abs(b-a)/2;
+end
+
